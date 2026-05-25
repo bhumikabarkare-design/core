1cd9c7fe2566777b4054c406a5ad2b36939b10a8
Bhumika Haribhai Barkare <bhumikabarkare@gmail.com>
2026-05-25 17:00:27 +0530

    updated code

--- home/views.py
+++ home/views.py
@@ -5,3 +5,9 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Hello, World!")
+
+def about(request):
+    return HttpResponse("This is the about page.")
+
+def contact(request):
+    return HttpResponse("This is the contact page.")
